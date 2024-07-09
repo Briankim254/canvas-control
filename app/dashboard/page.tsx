b687868b69ccd3a2ff1395be66a08ea521942d18
Brian Kimutai <kimutaibrian560@gmail.com>
2024-07-10 02:12:58 +0300

    feat: Add recharts package for radar chart component

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import Sidebar from '@/components/sidebar'
+import { Component } from '@/components/charts/test'
 
 const page=()=> {
   return (
-    <div>
+    <div className="flex">
       <Sidebar />
+      <main className="flex-1 p-4 bg-gray-100 dark:bg-gray-800 " >
+      <h3 className="scroll-m-20 text-4xl font-extrabold tracking-tight lg:text-5xl">
+        Dashboard
+      </h3>
+      <p className="leading-7 [&:not(:first-child)]:mt-6">
+        Welcome back!
+      </p>
+    
+      {/* two cards equally placed in a 4*4 grid */}
+
+      <div className="grid grid-cols-1 gap-4 mt-6 md:grid-cols-2">
+        <div className="p-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
+          <h4 className="text-lg font-semibold text-gray-800 dark:text-gray-100">
+            Total Visitors
+          </h4>
+          <p className="mt-2 text-3xl font-bold">
+            12, 678
+          </p>
+        </div>
+        <div className="p-4 bg-white rounded-lg shadow-md dark:bg-gray-800">
+          <h4 className="text-lg font-semibold text-gray-800 dark:text-gray-100">
+            Total Sales
+          </h4>
+          <p className="mt-2 text-3xl font-bold">
+            $ 89, 765
+          </p>
+        </div>
+      </div>
+
+      <Component />
+    </main>
     </div>
   )
 }
