65d935044af2c0d2c8254231f08d1f3b0c12f6f7
Brian Kimutai <kimutaibrian560@gmail.com>
2024-07-11 11:55:00 +0300

    add:email log in wirh magic links

--- auth.ts
+++ auth.ts
@@ -1,15 +1,17 @@
-import NextAuth from "next-auth"
-import { PrismaAdapter } from "@auth/prisma-adapter"
-import { PrismaClient } from "@prisma/client"
-import Resend from "next-auth/providers/resend"
-import prisma from "./prisma/client"
-import GitHub from "next-auth/providers/github"
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import Resend from "next-auth/providers/resend";
+import prisma from "./prisma/client";
+import GitHub from "next-auth/providers/github";
+import resend from "next-auth/providers/resend";
 
-
- 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma ),
+  adapter: PrismaAdapter(prisma),
   providers: [
-    GitHub
+    GitHub,
+    Resend({
+      from: "auth@kimutai.xyz",
+    }),
   ],
-}) 
+});
