366e8727cc7df7d89c681152f5a26d3ac942b4c4
Brian Kimutai <kimutaibrian560@gmail.com>
2024-07-16 22:28:04 +0300

    chore: Update npm dependencies to latest versions and fix type error in auth.ts

--- auth.ts
+++ auth.ts
@@ -2,9 +2,12 @@ import NextAuth from "next-auth";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import Resend from "next-auth/providers/resend";
 import prisma from "./prisma/client";
+import type { Adapter } from "next-auth/adapters";
+import { type DefaultSession } from "next-auth";
+import type { verification, Role } from "@prisma/client";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-  adapter: PrismaAdapter(prisma),
+  adapter: PrismaAdapter(prisma) as Adapter,
   providers: [
     Resend({
       from: "Auth@kimutai.xyz",
@@ -13,9 +16,14 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
   ],
   callbacks: {
     session({ session, user }) {
-      session.user.role = user.role;
-      session.user.verification = user.verification;
-      return session;
+      return {
+        ...session,
+        user: {
+          ...session.user,
+          role: user.role,
+          verification: user.verification,
+        },
+      };
     },
   },
 });
