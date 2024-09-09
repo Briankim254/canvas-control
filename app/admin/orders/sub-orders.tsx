4a4c4378526f64eafaa1f96a6b4286ccee7f9383
Brian Kimutai <kimutaibrian560@gmail.com>
2024-09-09 10:33:10 +0300

    Add "no-cors" mode to API request headers

--- app/admin/orders/sub-orders.tsx
+++ app/admin/orders/sub-orders.tsx
@@ -77,6 +77,7 @@ export default function SubOrders({
           {
             headers: {
               Authorization: `Bearer ${user?.token}` || "",
+              mode: "no-cors",
             },
           }
         );
